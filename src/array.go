ebaf5b2de118c6a40e46352428bc4d03b9a41a8a
liujb <liujiangbei@126.com>
2015-10-08 14:19:26 +0800

    modify

--- src/array.go
+++ src/array.go
@@ -7,9 +7,8 @@ func testArray() {
 	// 变长数组
 	array := [...]int{12, 23, 34, 56, 4, 56, 45, 6, 46, 4, 6, 54, 64, 6, 4, 64, 64, 64, 6, 4, 64, 6, 12}
 	for i := 0; i < len(array); i++ {
-		fmt.Println(array[i])
+		fmt.Println("number", array[i])
 	}
-
 }
 
 func main() {
--- src/comma-ok.go
+++ src/comma-ok.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func main() {
+
+	// 为map分配内存
 	mapSS := make(map[string]string)
+
 	mapSS["one"] = "string"
 	if _, ok := mapSS["one"]; ok {
 		fmt.Println(ok)
 	}
+
 }
